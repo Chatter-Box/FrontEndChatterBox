144da4cc34fe90885a4788a4b3626c4a2bdc1a43
TatianaDeAngelo <85692272+TatianaDeAngelo@users.noreply.github.com>
2021-08-26 20:00:24 -0400

    work plz

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+const [input, setInput] = useState('');
+const [messages, setMessages] = useState(['hello', 'Hi', 'Whats up']);
+
+console.log(input); //this allowed us to have messages in the console
+console.log(messages);
+
+
+const sendMessage = (event) => {
+  // all the logic to send a message goes here 
+  setMessages([...messages, input]) //this adds the new message to the end of the array
+  setInput(''); //clear input so that when the message bar is cleared
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello ChatterBox Chatters! </h1>
+
+      <input value={input} onChange = {event => setInput(event.target.value)} /> 
+      <button onClick ={sendMessage}> Send Message</button>
+     
+
+
+      
+      {
+        messages.map(message => (
+          <p>{message}</p>
+        ))
+      }
+      
     </div>
+
   );
 }
 
 export default App;
+
