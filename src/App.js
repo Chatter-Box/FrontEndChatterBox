ec8af0bcb34dcc82c09b04868ea998168b519829
TatianaDeAngelo <85692272+TatianaDeAngelo@users.noreply.github.com>
2021-08-28 09:34:33 -0400

    chat app updated

--- src/App.js
+++ src/App.js
@@ -1,40 +1,72 @@
-import React, { useState } from 'react';
-import './App.css';
+  import React, { useState, useEffect } from 'react';
+  import { Button, FormControl, InputLabel, Input } from '@material-ui/core';
+  import './App.css';
+  import Message from './Message';
+  import db from './firebase';
 
-function App() {
-const [input, setInput] = useState('');
-const [messages, setMessages] = useState(['hello', 'Hi', 'Whats up']);
+  //why are we using REACT instead of Angular? React is much more popular, it is growing and very in demand, it is super light weight
+  //the angular framebook needs a lot more to get it going, everything in angular can be done with react 
+  //using material UI because goog and more modern 
+  function App() {
+  const [input, setInput] = useState('');
+  const [messages, setMessages] = useState([{username: 'tatiana', message: 'hey'}, {username: 'miles', message: 'hello'} ]); //objects so that they can have a user and piece of text
+  //in order to store anything we always need to think about state
+  const [username, setUsername] = useState(''); //start with blank string because no user will have a name beforehand and this was setting up a hook 
+  //need another hook called userEffect this allows the code to run once when the component loads
+  //difference btwn useState and useEffect, the useState sets up a kind of short term memory when page is restarted and is a way to set up variable in react
+  //useEffect is a block of code that gets executed based on a condition 
+  //you can have several useEffect, many that you need for specified condition
 
-console.log(input); //this allowed us to have messages in the console
-console.log(messages);
+  useEffect(() => {
+  //run once when the app component loads
+  //all the documents are held inside snapshot below (docs are metadata)
+  //iterating through each document and getting the data from that document 
+    db.collection('messages').onSnapshot(snapshot => {
+      setMessages(snapshot.docs.map(doc => doc.data()))  
+    })
+  }, [] )
 
+  useEffect(() => {
+    //run code here
+    setUsername(prompt('Please enter your name')); 
+  }, [] ) //condition, map this to a variable, if blank it runs code once when app component loads, if input now everytime input changes the code runs everytime
 
-const sendMessage = (event) => {
-  // all the logic to send a message goes here 
-  setMessages([...messages, input]) //this adds the new message to the end of the array
-  setInput(''); //clear input so that when the message bar is cleared
-}
+  const sendMessage = (event) => {
+    // all the logic to send a message goes here 
+    event.preventDefault(); //prevents the refresh from taking place upon hitting enter or "send message" button
+    setMessages([...messages, {username: username, text: input}]) //this adds the new message to the end of the array
+    setInput(''); //clear input so that when the message bar is cleared
+  }
 
-  return (
-    <div className="App">
-      <h1>Hello ChatterBox Chatters! </h1>
+  //below I have added capital b Button to change the look of the button, I had to add Material UI
+  //Can go to the https://material-ui.com/components/buttons/
+  //disabled prevents sending empty
+  //components are a powerful part of react, resuable allows you to write it once, can pass 'props' (properties) in and it allows you to 
+  //change message contents
+    return (
+      <div className="App">
+        <h1>Hello ChatterBox Chatters!</h1>
+        <h2>Welcome {username}</h2>
 
-      <input value={input} onChange = {event => setInput(event.target.value)} /> 
-      <button onClick ={sendMessage}> Send Message</button>
-     
+        <form>
 
+        <FormControl>
+          <InputLabel >Message...</InputLabel>
+          <Input value={input} onChange = {event => setInput(event.target.value)}/>
+          <Button disabled={!input} variant="contained" color="primary" type='submit' onClick={sendMessage}> Send </Button>
+        </FormControl>       
+        </form>
+        
+          
+        {
+          messages.map(message => (
+            <Message username={username} message={message}/> //the person who has logged in and their message
+          ))
+        }
+      </div>
 
-      
-      {
-        messages.map(message => (
-          <p>{message}</p>
-        ))
-      }
-      
-    </div>
+    );
+  }
 
-  );
-}
-
-export default App;
+  export default App;
 
---
+++ src/Message.js
@@ -0,0 +1,31 @@
+//es7 snippets
+//this is the styling for the messages
+import React from 'react';
+import { Card, CardContent, Typography } from '@material-ui/core';
+import './Message.css';
+
+
+function Message({message, username}) {
+    const isUser = username === message.username; //is this the user who has logged in, if the username is equal to the message username then code below would be true
+    //changing so that each user has different styling, below changed className
+    //the if then statement allows us to have if logged in use userCard styling if not then use guest, using BEM convention styling
+    //first part is component and second is element 
+    //set up database using firestore (firebase) this also allows hosting online 
+        return (
+            <div className={`message ${isUser && 'message__user'}`}>
+                <Card className={isUser ? "message__userCard" : "message__guestCard"}> 
+                    <CardContent>
+                        <Typography 
+                            color ="white" 
+                            variant= "h5"
+                            component="h2"
+                        >
+                            {message.username}: {message.message}
+                        </Typography>
+                    </CardContent>
+                </Card>
+                </div>
+        )
+}
+
+export default Message
