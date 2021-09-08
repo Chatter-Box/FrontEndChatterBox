09325e174d31ee6aa2dd25fe49a3d1d834a55f88
Zachary Kitto <85901882+ZachKitto@users.noreply.github.com>
2021-09-08 08:28:34 -0400

    Revert "Feature/registration"

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -1,8 +1,7 @@
 import { FormControl, FormHelperText, Input, InputLabel, Grid, TextField, Button, Link, Paper } from '@material-ui/core';
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import PersonIcon from '@material-ui/icons/Person';
 import { useHistory } from 'react-router';
-import axios from 'axios';
 import './login.css';
 
 export default function Login() {
@@ -10,29 +9,13 @@ export default function Login() {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
     const history = useHistory();
-    const [messages, setMessages] = useState({});
-    const [isLoading, setLoading] = useState(true);
-    const [wrongCredentials, setWrongCredentials] = useState(false);
 
-    const login = async (event) => {
+    const checkIfUserExists = (event) => {
         event.preventDefault();
         // look for the users info in the database
-        try {
-            const response = await axios.post(`/profile/login/${username}/${password}`);
-            if (response.data.token) {
-            localStorage.setItem('user', JSON.stringify(response.data));
-            console.log(localStorage.getItem('profile'));
-            // Take to profile
-        } 
-        } catch (error) {
-            setWrongCredentials(true);
-            setUsername('');
-            setPassword('');
-        }
-        
-        // fetch(`/profile/login/${username}/${password}`)
-        //     .then(response => response.json())
-        //     .then(body => body.username === username ? takeUserToProfile() : wrongUsernameOrPassword());
+        fetch(`/profile/login/${username}/${password}`)
+            .then(response => response.json())
+            .then(body => body.username === username ? takeUserToProfile() : wrongUsernameOrPassword());
     }
 
     const takeUserToProfile = () => {
@@ -52,31 +35,19 @@ export default function Login() {
         history.push('/register');
     }
 
-    // useEffect(() => {
-    //     axios.get('/message/all')
-    //         .then(response => setMessages(response.data))
-    //         .finally(() => setLoading(false));
-    // },[] )
-
     const goToAboutUs= (event) => {
         event.preventDefault();
         history.push('/aboutUs');
     }
+
     
     console.log(username);
     console.log(password);
 
-    // if (isLoading) {
-    //     return <p>Loading...</p>
-    // }
-
     return (
         <Grid align='center'>
             <h1>Welcome to Chatter-Box!</h1>
-            <p>{console.log(messages)}</p>
-            {/* {messages.map(message => <p key={message.id}>{message.body}</p>)} */}
             <Paper className='paper__style' variant='outlined' elevation={10}>
-                {wrongCredentials && <p className='invalid__credentials'>Invalid username or password</p>}
                 <TextField className='item__position' fullWidth
                     value={username} onChange={(input) => setUsername(input.target.value)} 
                     label='Username' placeholder='Enter Username'/> <br/><br/>
@@ -85,7 +56,7 @@ export default function Login() {
                     value={password} onChange={(input) => setPassword(input.target.value)} 
                     label='Password' placeholder='Enter Password' type='password'/> <br/><br/>
             
-                <Button className='item__position' fullWidth variant='contained' disabled={!username || !password} onClick={login} size='medium' color='primary'>Login</Button> <br/><br/><br/>
+                <Button className='item__position' fullWidth variant='contained' disabled={!username || !password} onClick={checkIfUserExists} size='medium' color='primary'>Login</Button> <br/><br/><br/>
 
                 <Link className='item__position link' onClick={goToRegistration}>Create a New Account</Link> <br/>
             </Paper>
